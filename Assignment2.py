c363c27258694855c6b7430895a78caed1b2d035
Josh <joshuayeboah456@gmail.com>
2025-07-21 00:05:36 +0000

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,64 @@
+#For multiples of 2
+print("Multiples of 2:")
+print("\t1")
+for i in range(2,25,2):
+    print(f"\t{i}")
+
+#For multiples of 3
+print("Multiples of 3:")
+print("\t1")
+for i in range(3,37,3):
+    print(f"\t{i}")
+#For multiples of 4
+print("Multiples of 4:")
+print("\t1")
+for i in range(4,49,4):
+    print(f"\t{i}")
+
+#For multiples of 5
+print("Multiples of 5:")
+print("\t1")
+for i in range(5,61,5):
+    print(f"\t{i}")
+
+#For multiples of 6
+print("Multiples of 6 :")
+print("\t1")
+for i in range(6,73,6):
+    print(f"\t{i}")
+
+#For multiples of 7
+print("Multiples of 7:")
+print("\t1")
+for i in range(7,85,7):
+    print(f"\t{i}")    
+
+#For multiples of 8
+print("Multiples of 8:")
+print("\t1")
+for i in range(8,97,8):
+    print(f"\t{i}")
+
+#For multiples of 9
+print("Multiples of 9:")
+print("\t1")
+for i in range(9,109,9):
+    print(f"\t{i}")    
+
+#For multiples of 10
+print("Multiples of 10:")
+print("\t1")
+for i in range(10,121,10):
+    print(f"\t{i}")
+
+#For multiples of 11
+print("Multiples of 11:")
+print("\t1")
+for i in range(11,133,11):
+    print(f"\t{i}")
+
+#For multiples of 12
+print("Multiples of 12:")
+print("\t1")
+for i in range(12,145,12):
+    print(f"\t{i}")
