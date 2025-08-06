431b388b9f98fb032f5b67bbaf22d8940911287e
Josh <joshuayeboah456@gmail.com>
2025-08-06 11:48:15 +0000

    Add files via upload

---
+++ Assignment2(1).py
@@ -0,0 +1,7 @@
+# Multiplication Table
+#I found a way to make aasignment 2 code shorter
+
+for i in range (1,13):
+    print (f"MULTIPLICATION {i} :")
+    for j in range (1,13):
+        print( f"\t{j*i}")
